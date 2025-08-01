96d23958e48b3c3d012349e3310a68ba6d8a11d8
Sarah Norris <sarah@sekai.co.uk>
2025-08-01 11:54:50 +0100

    Move all inspector controls into TermsQueryInspectorControls

--- packages/block-library/src/terms-query/inspector-controls.js
+++ packages/block-library/src/terms-query/inspector-controls.js
@@ -8,6 +8,10 @@ import {
 	SelectControl,
 	ToggleControl,
 } from '@wordpress/components';
+import {
+	InspectorControls,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 
@@ -15,11 +19,16 @@ import { store as coreStore } from '@wordpress/core-data';
  * Internal dependencies
  */
 import { useToolsPanelDropdownMenuProps } from '../utils/hooks';
+import { unlock } from '../lock-unlock';
+
+const { HTMLElementControl } = unlock( blockEditorPrivateApis );
 
 export default function TermsQueryInspectorControls( {
 	attributes,
 	setQuery,
 	setAttributes,
+	TagName,
+	clientId,
 } ) {
 	const { termQuery } = attributes;
 	const dropdownMenuProps = useToolsPanelDropdownMenuProps();
@@ -39,133 +48,156 @@ export default function TermsQueryInspectorControls( {
 	} ) );
 
 	return (
-		<ToolsPanel
-			label={ __( 'Terms Query Settings' ) }
-			resetAll={ () => {
-				setAttributes( {
-					termQuery: {
-						taxonomy: 'category',
-						order: 'asc',
-						orderBy: 'name',
-						hideEmpty: true,
-						hierarchical: false,
-					},
-				} );
-			} }
-			dropdownMenuProps={ dropdownMenuProps }
-		>
-			<ToolsPanelItem
-				hasValue={ () => termQuery.taxonomy !== 'category' }
-				label={ __( 'Taxonomy' ) }
-				onDeselect={ () => setQuery( { taxonomy: 'category' } ) }
-				isShownByDefault
-			>
-				<SelectControl
-					__nextHasNoMarginBottom
-					__next40pxDefaultSize
-					label={ __( 'Taxonomy' ) }
-					options={ taxonomyOptions }
-					value={ termQuery.taxonomy }
-					onChange={ ( selectedTaxonomy ) =>
-						setQuery( { taxonomy: selectedTaxonomy } )
-					}
-				/>
-			</ToolsPanelItem>
-
-			<ToolsPanelItem
-				hasValue={ () => termQuery.order !== 'asc' }
-				label={ __( 'Order' ) }
-				onDeselect={ () => setQuery( { order: 'asc' } ) }
-				isShownByDefault
-			>
-				<SelectControl
-					__nextHasNoMarginBottom
-					__next40pxDefaultSize
-					label={ __( 'Order' ) }
-					options={ [
-						{ label: __( 'Ascending' ), value: 'asc' },
-						{ label: __( 'Descending' ), value: 'desc' },
-					] }
-					value={ termQuery.order }
-					onChange={ ( order ) => setQuery( { order } ) }
-				/>
-			</ToolsPanelItem>
-
-			<ToolsPanelItem
-				hasValue={ () => termQuery.orderBy !== 'name' }
-				label={ __( 'Order by' ) }
-				onDeselect={ () => setQuery( { orderBy: 'name' } ) }
-				isShownByDefault
-			>
-				<SelectControl
-					__nextHasNoMarginBottom
-					__next40pxDefaultSize
-					label={ __( 'Order by' ) }
-					options={ [
-						{ label: __( 'Name' ), value: 'name' },
-						{ label: __( 'Slug' ), value: 'slug' },
-						{ label: __( 'Count' ), value: 'count' },
-					] }
-					value={ termQuery.orderBy }
-					onChange={ ( orderBy ) => setQuery( { orderBy } ) }
-				/>
-			</ToolsPanelItem>
-
-			<ToolsPanelItem
-				hasValue={ () => termQuery.parent === 0 }
-				label={ __( 'Show only top level terms' ) }
-				onDeselect={ () => setQuery( { parent: undefined } ) }
-				isShownByDefault
-			>
-				<ToggleControl
-					__nextHasNoMarginBottom
-					label={ __( 'Show only top level terms' ) }
-					checked={ termQuery.parent === 0 }
-					onChange={ ( showTopLevel ) => {
-						setQuery( { parent: showTopLevel ? 0 : undefined } );
-						if ( showTopLevel && termQuery.hierarchical ) {
-							setQuery( { hierarchical: false } );
-						}
+		<>
+			<InspectorControls>
+				<ToolsPanel
+					label={ __( 'Terms Query Settings' ) }
+					resetAll={ () => {
+						setAttributes( {
+							termQuery: {
+								taxonomy: 'category',
+								order: 'asc',
+								orderBy: 'name',
+								hideEmpty: true,
+								hierarchical: false,
+							},
+						} );
 					} }
-					disabled={ !! termQuery.hierarchical }
-				/>
-			</ToolsPanelItem>
-
-			<ToolsPanelItem
-				hasValue={ () => termQuery.hideEmpty !== true }
-				label={ __( 'Show empty terms' ) }
-				onDeselect={ () => setQuery( { hideEmpty: true } ) }
-				isShownByDefault
-			>
-				<ToggleControl
-					__nextHasNoMarginBottom
-					label={ __( 'Show empty terms' ) }
-					checked={ ! termQuery.hideEmpty }
-					onChange={ ( showEmpty ) =>
-						setQuery( { hideEmpty: ! showEmpty } )
-					}
-				/>
-			</ToolsPanelItem>
-
-			<ToolsPanelItem
-				hasValue={ () => termQuery.hierarchical !== false }
-				label={ __( 'Show hierarchy' ) }
-				onDeselect={ () => setQuery( { hierarchical: false } ) }
-				isShownByDefault
-			>
-				<ToggleControl
-					__nextHasNoMarginBottom
-					label={ __( 'Show hierarchy' ) }
-					checked={ termQuery.hierarchical }
-					onChange={ ( hierarchical ) => {
-						setQuery( { hierarchical } );
-						if ( hierarchical && termQuery.parent ) {
-							setQuery( { parent: 0 } );
+					dropdownMenuProps={ dropdownMenuProps }
+				>
+					<ToolsPanelItem
+						hasValue={ () => termQuery.taxonomy !== 'category' }
+						label={ __( 'Taxonomy' ) }
+						onDeselect={ () =>
+							setQuery( { taxonomy: 'category' } )
 						}
-					} }
-					disabled={ termQuery.parent === 0 }
+						isShownByDefault
+					>
+						<SelectControl
+							__nextHasNoMarginBottom
+							__next40pxDefaultSize
+							label={ __( 'Taxonomy' ) }
+							options={ taxonomyOptions }
+							value={ termQuery.taxonomy }
+							onChange={ ( selectedTaxonomy ) =>
+								setQuery( { taxonomy: selectedTaxonomy } )
+							}
+						/>
+					</ToolsPanelItem>
+
+					<ToolsPanelItem
+						hasValue={ () => termQuery.order !== 'asc' }
+						label={ __( 'Order' ) }
+						onDeselect={ () => setQuery( { order: 'asc' } ) }
+						isShownByDefault
+					>
+						<SelectControl
+							__nextHasNoMarginBottom
+							__next40pxDefaultSize
+							label={ __( 'Order' ) }
+							options={ [
+								{ label: __( 'Ascending' ), value: 'asc' },
+								{ label: __( 'Descending' ), value: 'desc' },
+							] }
+							value={ termQuery.order }
+							onChange={ ( order ) => setQuery( { order } ) }
+						/>
+					</ToolsPanelItem>
+
+					<ToolsPanelItem
+						hasValue={ () => termQuery.orderBy !== 'name' }
+						label={ __( 'Order by' ) }
+						onDeselect={ () => setQuery( { orderBy: 'name' } ) }
+						isShownByDefault
+					>
+						<SelectControl
+							__nextHasNoMarginBottom
+							__next40pxDefaultSize
+							label={ __( 'Order by' ) }
+							options={ [
+								{ label: __( 'Name' ), value: 'name' },
+								{ label: __( 'Slug' ), value: 'slug' },
+								{ label: __( 'Count' ), value: 'count' },
+							] }
+							value={ termQuery.orderBy }
+							onChange={ ( orderBy ) => setQuery( { orderBy } ) }
+						/>
+					</ToolsPanelItem>
+
+					<ToolsPanelItem
+						hasValue={ () => termQuery.parent === 0 }
+						label={ __( 'Show only top level terms' ) }
+						onDeselect={ () => setQuery( { parent: undefined } ) }
+						isShownByDefault
+					>
+						<ToggleControl
+							__nextHasNoMarginBottom
+							label={ __( 'Show only top level terms' ) }
+							checked={ termQuery.parent === 0 }
+							onChange={ ( showTopLevel ) => {
+								setQuery( {
+									parent: showTopLevel ? 0 : undefined,
+								} );
+								if ( showTopLevel && termQuery.hierarchical ) {
+									setQuery( { hierarchical: false } );
+								}
+							} }
+							disabled={ !! termQuery.hierarchical }
+						/>
+					</ToolsPanelItem>
+
+					<ToolsPanelItem
+						hasValue={ () => termQuery.hideEmpty !== true }
+						label={ __( 'Show empty terms' ) }
+						onDeselect={ () => setQuery( { hideEmpty: true } ) }
+						isShownByDefault
+					>
+						<ToggleControl
+							__nextHasNoMarginBottom
+							label={ __( 'Show empty terms' ) }
+							checked={ ! termQuery.hideEmpty }
+							onChange={ ( showEmpty ) =>
+								setQuery( { hideEmpty: ! showEmpty } )
+							}
+						/>
+					</ToolsPanelItem>
+
+					<ToolsPanelItem
+						hasValue={ () => termQuery.hierarchical !== false }
+						label={ __( 'Show hierarchy' ) }
+						onDeselect={ () => setQuery( { hierarchical: false } ) }
+						isShownByDefault
+					>
+						<ToggleControl
+							__nextHasNoMarginBottom
+							label={ __( 'Show hierarchy' ) }
+							checked={ termQuery.hierarchical }
+							onChange={ ( hierarchical ) => {
+								setQuery( { hierarchical } );
+								if ( hierarchical && termQuery.parent ) {
+									setQuery( { parent: 0 } );
+								}
+							} }
+							disabled={ termQuery.parent === 0 }
+						/>
+					</ToolsPanelItem>
+				</ToolsPanel>
+			</InspectorControls>
+			<InspectorControls group="advanced">
+				<HTMLElementControl
+					tagName={ TagName }
+					onChange={ ( value ) =>
+						setAttributes( { tagName: value } )
+					}
+					clientId={ clientId }
+					options={ [
+						{ label: __( 'Default (<div>)' ), value: 'div' },
+						{ label: '<main>', value: 'main' },
+						{ label: '<section>', value: 'section' },
+						{ label: '<aside>', value: 'aside' },
+					] }
 				/>
-			</ToolsPanelItem>
-		</ToolsPanel>
+			</InspectorControls>
+		</>
 	);
 }
--- packages/block-library/src/terms-query/terms-query-content.js
+++ packages/block-library/src/terms-query/terms-query-content.js
@@ -5,21 +5,15 @@ import { useDispatch } from '@wordpress/data';
 import { useInstanceId } from '@wordpress/compose';
 import { useEffect } from '@wordpress/element';
 import {
-	InspectorControls,
 	useBlockProps,
 	store as blockEditorStore,
 	useInnerBlocksProps,
-	privateApis as blockEditorPrivateApis,
 } from '@wordpress/block-editor';
-import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { unlock } from '../lock-unlock';
 import TermsQueryInspectorControls from './inspector-controls';
-
-const { HTMLElementControl } = unlock( blockEditorPrivateApis );
 
 export default function TermsQueryContent( {
 	attributes,
@@ -62,30 +56,14 @@ export default function TermsQueryContent( {
 
 	return (
 		<>
-			<InspectorControls>
-				<TermsQueryInspectorControls
-					name={ name }
-					attributes={ attributes }
-					setQuery={ setQuery }
-					setAttributes={ setAttributes }
-					clientId={ clientId }
-				/>
-			</InspectorControls>
-			<InspectorControls group="advanced">
-				<HTMLElementControl
-					tagName={ TagName }
-					onChange={ ( value ) =>
-						setAttributes( { tagName: value } )
-					}
-					clientId={ clientId }
-					options={ [
-						{ label: __( 'Default (<div>)' ), value: 'div' },
-						{ label: '<main>', value: 'main' },
-						{ label: '<section>', value: 'section' },
-						{ label: '<aside>', value: 'aside' },
-					] }
-				/>
-			</InspectorControls>
+			<TermsQueryInspectorControls
+				name={ name }
+				attributes={ attributes }
+				setQuery={ setQuery }
+				setAttributes={ setAttributes }
+				clientId={ clientId }
+				tagName={ TagName }
+			/>
 			<TagName { ...innerBlocksProps } />
 		</>
 	);
