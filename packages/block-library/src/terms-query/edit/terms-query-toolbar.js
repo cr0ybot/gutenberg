8a01b38669190d85aaf7ab4a67f9a8144fab1e58
Cory Hughart <cory@alphaparticle.com>
2025-08-27 07:27:01 -0700

    add button to toolbar for choosing a different pattern after initial setting

---
+++ packages/block-library/src/terms-query/edit/terms-query-toolbar.js
@@ -0,0 +1,44 @@
+/**
+ * WordPress dependencies
+ */
+import { ToolbarGroup, ToolbarButton, Dropdown } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import PatternSelection, { useBlockPatterns } from './pattern-selection';
+
+export default function TermsQueryToolbar( { clientId, attributes } ) {
+	const hasPatterns = useBlockPatterns( clientId, attributes ).length > 1;
+	if ( ! hasPatterns ) {
+		return null;
+	}
+
+	return (
+		<ToolbarGroup className="wp-block-template-part__block-control-group">
+			<Dropdown
+				contentClassName="block-editor-block-settings-menu__popover"
+				focusOnMount="firstElement"
+				expandOnMobile
+				renderToggle={ ( { isOpen, onToggle } ) => (
+					<ToolbarButton
+						aria-haspopup="true"
+						aria-expanded={ isOpen }
+						onClick={ onToggle }
+					>
+						{ __( 'Change design' ) }
+					</ToolbarButton>
+				) }
+				renderContent={ () => (
+					<PatternSelection
+						clientId={ clientId }
+						attributes={ attributes }
+						showSearch={ false }
+						showTitlesAsTooltip
+					/>
+				) }
+			/>
+		</ToolbarGroup>
+	);
+}
