e474d2b1d40403b5d21943469ba3ce93d2004760
Cory Hughart <cory@alphaparticle.com>
2025-09-05 15:36:22 -0500

    update terms query toolbar to match query block

--- packages/block-library/src/terms-query/edit/terms-query-toolbar.js
+++ packages/block-library/src/terms-query/edit/terms-query-toolbar.js
@@ -1,7 +1,12 @@
 /**
  * WordPress dependencies
  */
-import { ToolbarGroup, ToolbarButton, Dropdown } from '@wordpress/components';
+import {
+	ToolbarGroup,
+	ToolbarButton,
+	Dropdown,
+	__experimentalDropdownContentWrapper as DropdownContentWrapper,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -9,36 +14,46 @@ import { __ } from '@wordpress/i18n';
  */
 import PatternSelection, { useBlockPatterns } from './pattern-selection';
 
-export default function TermsQueryToolbar( { clientId, attributes } ) {
-	const hasPatterns = useBlockPatterns( clientId, attributes ).length > 1;
+export default function TermsQueryToolbar( {
+	clientId,
+	attributes,
+	hasInnerBlocks,
+} ) {
+	const hasPatterns = useBlockPatterns( clientId, attributes ).length;
 	if ( ! hasPatterns ) {
 		return null;
 	}
 
+	const buttonLabel = hasInnerBlocks
+		? __( 'Change design' )
+		: __( 'Choose pattern' );
+
 	return (
 		<ToolbarGroup className="wp-block-template-part__block-control-group">
-			<Dropdown
-				contentClassName="block-editor-block-settings-menu__popover"
-				focusOnMount="firstElement"
-				expandOnMobile
-				renderToggle={ ( { isOpen, onToggle } ) => (
-					<ToolbarButton
-						aria-haspopup="true"
-						aria-expanded={ isOpen }
-						onClick={ onToggle }
-					>
-						{ __( 'Change design' ) }
-					</ToolbarButton>
-				) }
-				renderContent={ () => (
-					<PatternSelection
-						clientId={ clientId }
-						attributes={ attributes }
-						showSearch={ false }
-						showTitlesAsTooltip
-					/>
-				) }
-			/>
+			<DropdownContentWrapper>
+				<Dropdown
+					contentClassName="block-editor-block-settings-menu__popover"
+					focusOnMount="firstElement"
+					expandOnMobile
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<ToolbarButton
+							aria-haspopup="true"
+							aria-expanded={ isOpen }
+							onClick={ onToggle }
+						>
+							{ buttonLabel }
+						</ToolbarButton>
+					) }
+					renderContent={ () => (
+						<PatternSelection
+							clientId={ clientId }
+							attributes={ attributes }
+							showSearch={ false }
+							showTitlesAsTooltip
+						/>
+					) }
+				/>
+			</DropdownContentWrapper>
 		</ToolbarGroup>
 	);
 }
