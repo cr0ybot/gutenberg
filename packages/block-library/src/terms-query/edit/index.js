d2259395dd66ca72b308b070a5306aa5a0474b1a
Cory Hughart <cory@coryhughart.com>
2025-09-21 14:51:19 -0500

    remove forced termQuery inheritance

--- packages/block-library/src/terms-query/edit/index.js
+++ packages/block-library/src/terms-query/edit/index.js
@@ -13,23 +13,18 @@ import TermsQueryPlaceholder from './terms-query-placeholder';
 import { PatternSelectionModal } from './pattern-selection';
 
 const TermsQueryEdit = ( props ) => {
-	const { attributes, clientId, context } = props;
-	const { termQuery } = attributes;
+	const { attributes, clientId } = props;
 	const [ isPatternSelectionModalOpen, setIsPatternSelectionModalOpen ] =
 		useState( false );
-	const { termQuery: queryContext } = context;
-	// Force inherit if context is present.
-	const inherit = !! ( queryContext ?? false ) || termQuery?.inherit;
-	const taxonomy = inherit ? queryContext?.taxonomy : termQuery?.taxonomy;
-
 	const hasInnerBlocks = useSelect(
 		( select ) =>
 			!! select( blockEditorStore ).getBlocks( clientId ).length,
 		[ clientId ]
 	);
+	const Component = hasInnerBlocks
+		? TermsQueryContent
+		: TermsQueryPlaceholder;
 
-	const Component =
-		taxonomy && hasInnerBlocks ? TermsQueryContent : TermsQueryPlaceholder;
 	return (
 		<>
 			<Component
