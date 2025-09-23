e95a17ee5c3a33b845242a80641ff5fcf1c10081
Cory Hughart <cory@coryhughart.com>
2025-09-22 23:29:51 -0500

    Render just the term template block directly with dynamic=false

--- packages/block-library/src/term-template/index.php
+++ packages/block-library/src/term-template/index.php
@@ -21,17 +21,14 @@ function render_block_core_term_template( $attributes, $content, $block ) {
 		return '';
 	}
 
-	$query_block_context = $block->context;
-
-	if ( empty( $query_block_context['termQuery'] ) ) {
+	if ( empty( $block->context['termQuery'] ) ) {
 		return '';
 	}
 
-	$query      = $query_block_context['termQuery'];
 	$query_args = gutenberg_build_query_vars_from_terms_query_block( $block );
 
 	// Inherit by default if termId is available in context.
-	$inherit      = isset( $block->context['query']['inherit'] ) ? $block->context['query']['inherit'] : isset( $block->context['termId'] );
+	$inherit      = isset( $block->context['termQuery']['inherit'] ) ? $block->context['termQuery']['inherit'] : isset( $block->context['termId'] );
 	$inherit_from = 'none';
 
 	// Inheritance order: block context, post context, taxonomy archive context.
@@ -46,13 +43,11 @@ function render_block_core_term_template( $attributes, $content, $block ) {
 	if ( 'post' === $inherit_from ) {
 		// Collect a subset of args from the query.
 		$post_query_args = array(
-			'number'       => $query_args['number'],
-			'order'        => $query_args['order'],
-			'orderby'      => $query_args['orderby'],
-			'hide_empty'   => $query_args['hide_empty'],
-			'hierarchical' => $query_args['hierarchical'],
+			'number'  => $query_args['number'],
+			'order'   => $query_args['order'],
+			'orderby' => $query_args['orderby'],
 		);
-		$terms           = wp_get_post_terms( $block->context['postId'], $query['taxonomy'], $post_query_args );
+		$terms           = wp_get_post_terms( $block->context['postId'], $query_args['taxonomy'], $post_query_args );
 	} else {
 		if ( 'block' === $inherit_from ) {
 			// Get the parent term ID from the block context.
@@ -78,33 +73,41 @@ function render_block_core_term_template( $attributes, $content, $block ) {
 
 	$content = '';
 	foreach ( $terms as $term ) {
-		$inner_blocks  = $block->inner_blocks;
+		// Get an instance of the current Term Template block.
+		$block_instance = $block->parsed_block;
+
+		// Set the block name to one that does not correspond to an existing registered block.
+		// This ensures that for the inner instances of the Term Template block, we do not render any block supports.
+		$block_instance['blockName'] = 'core/null';
+
+		$term_id  = $term->term_id;
+		$taxonomy = $term->taxonomy;
+
+		$filter_block_context = static function ( $context ) use ( $term_id, $taxonomy ) {
+			$context['termId']   = $term_id;
+			$context['taxonomy'] = $taxonomy;
+			return $context;
+		};
+
 		$block_content = '';
 
-		if ( ! empty( $inner_blocks ) ) {
-			$term_id  = $term->term_id;
-			$taxonomy = $term->taxonomy;
 
-			$filter_block_context = static function ( $context ) use ( $term_id, $taxonomy ) {
-				$context['termId']   = $term_id;
-				$context['taxonomy'] = $taxonomy;
-				return $context;
-			};
+		// Use an early priority to so that other 'render_block_context' filters have access to the values.
+		add_filter( 'render_block_context', $filter_block_context, 1 );
 
-			add_filter( 'render_block_context', $filter_block_context, 1 );
-
-			foreach ( $inner_blocks as $inner_block ) {
-				if ( method_exists( $inner_block, 'refresh_context_dependents' ) ) {
-					// WP_Block::refresh_context_dependents() was introduced in WordPress 6.8.
-					$inner_block->refresh_context_dependents();
-					$block_content .= $inner_block->render( array( 'dynamic' => true ) );
-				} else {
-					$block_content = ( new WP_Block( $inner_block->parsed_block ) )->render( array( 'dynamic' => false ) );
-				}
-			}
-			remove_filter( 'render_block_context', $filter_block_context, 1 );
+		// Render the inner blocks of the Post Template block with `dynamic` set to `false` to prevent calling
+		// `render_callback` and ensure that no wrapper markup is included.
+		if ( method_exists( $block, 'refresh_context_dependents' ) ) {
+			// WP_Block::refresh_context_dependents() was introduced in WordPress 6.8.
+			$block->refresh_context_dependents();
+			$block_content .= $block->render( array( 'dynamic' => false ) );
+		} else {
+			$block_content .= ( new WP_Block( $block_instance ) )->render( array( 'dynamic' => false ) );
 		}
 
+		remove_filter( 'render_block_context', $filter_block_context, 1 );
+
+		// Wrap the render inner blocks in a `li` element with the appropriate term classes.
 		$term_classes = implode( ' ', array( 'wp-block-term', 'term-' . $term->term_id ) );
 
 		$content .= '<li class="' . esc_attr( $term_classes ) . '">' . $block_content . '</li>';
